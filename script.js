2c0b666b0ff822143ca0487ac08ebc3349768557
JKGrizzly <kevin.jakubec@hetic.net>
2016-02-07 18:31:18 +0100

    scroll cross

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
 
+	var cross = $('div.cross');
+		
+		var cross_top_initial = cross.offset().top;
+		var section = $('section').length;
+		var cross_height_max = 20 * (section - 1);
+		var height = $('body').height();
+
+
 	//openBurger();
 	closeBurger();
-	scrollEtape();
-	
+	scrollEtape();	
 	if (window.matchMedia("(max-width: 650px)").matches) {
 		openBurger('75%');
 	}
 	else{
 		openBurger('40%');
+	}
+
+	if(window.matchMedia("(min-width: 965px)").matches){
+		scrollCross(cross_top_initial, section, cross_height_max, height)
 	}
 
     //Open the menu
@@ -74,5 +85,70 @@ $(document).ready(function(){
 			}
 		});
 	}
+
+	
+
+	function scrollCross(cross_top_initial, section, cross_height_max, height){
+
+		$(document).on('scroll', function(){
+			var scroll_top = $(window).scrollTop();
+			var scroll = $(window).height();
+			var pos_scroll = (scroll_top / (height - scroll));
+			var pos_cross = cross_top_initial + pos_scroll * cross_height_max;
+
+			var etape = cross_height_max / section;
+
+			if(pos_cross < 138){
+				$('aside h2').html('');
+				$('aside h2').html('Etape 1');
+			}
+			else if(pos_cross < 156) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 2');
+			}
+			else if(pos_cross < 174) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 3');
+			}
+			else if(pos_cross < 192) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 4');
+			}
+			else if(pos_cross < 210) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 5');
+			}
+			else if(pos_cross < 228) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 6');
+			}
+			else if(pos_cross < 246) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 7');
+			}
+			else if(pos_cross < 264) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 8');
+			}
+			else if(pos_cross < 282) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 9');
+			}
+			else if(pos_cross > 282) {
+				$('aside h2').html('');
+				$('aside h2').html('Etape 10');
+			}
+
+			console.log(pos_cross);
+
+			cross.animate({
+				top: pos_cross
+			}, {
+				duration : 10
+			});
+
+		});
+	}
+
 
 });
